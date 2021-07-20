df4d3ca34165b2771d5f18134a80dba1f1d1b7f6
Martín Calderón <martincalderon92@outlook.com>
2021-07-20 19:29:01 -0300

    Add Try/Catch blocks.

--- src/routes/tasks_routes.js
+++ src/routes/tasks_routes.js
@@ -4,32 +4,52 @@ const router = express.Router();
 const Task = require('../models/tasks');
 
 router.get('/', async (req, res) => {
-	const tasks = await Task.find();
-	res.json(tasks);
+	try {
+		const tasks = await Task.find();
+		res.json(tasks);
+	} catch (err) {
+		console.log(err);
+	}
 });
 
 router.get('/:id', async (req, res) => {
-	const task = await Task.findById(req.params.id);
-	res.json(task);
+	try {
+		const task = await Task.findById(req.params.id);
+		res.json(task);
+	} catch (err) {
+		console.log(err);
+	}
 });
 
 router.post('/', async (req, res) => {
-	const { title, description } = req.body;
-	const task = new Task({ title, description });
-	await task.save();
-	res.json({ status: 'Task saved.' });
+	try {
+		const { title, description } = req.body;
+		const task = new Task({ title, description });
+		await task.save();
+		res.json({ status: 'Task saved.' });
+	} catch (err) {
+		console.log(err);
+	}
 });
 
 router.put('/:id', async (req, res) => {
-	const { title, description } = req.body;
-	const updatedTask = { title, description };
-	await Task.findByIdAndUpdate(req.params.id, updatedTask);
-	res.json({ status: 'Task updated.' });
+	try {
+		const { title, description } = req.body;
+		const updatedTask = { title, description };
+		await Task.findByIdAndUpdate(req.params.id, updatedTask);
+		res.json({ status: 'Task updated.' });
+	} catch (err) {
+		console.log(err);
+	}
 });
 
 router.delete('/:id', async (req, res) => {
-	await Task.findByIdAndRemove(req.params.id);
-	res.json({ status: 'Task deleted.' });
+	try {
+		await Task.findByIdAndRemove(req.params.id);
+		res.json({ status: 'Task deleted.' });
+	} catch (err) {
+		console.log(err);
+	}
 });
 
 module.exports = router;
