080b6cecfcb498ae7586e8e4da7058fd3a49cf42
Martín Calderón <martincalderon92@outlook.com>
2021-07-20 20:57:58 -0300

    Hide MongoDB URI.

--- src/connection.js
+++ src/connection.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
-const URI =
-	'mongodb+srv://mdcp:Xq9Ga9AzLvb8abr@cluster0.mnqn4.mongodb.net/mern-todo-app-database?retryWrites=true&w=majority';
+const URI = process.env.MONGODB_URI;
 
 mongoose
 	.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
