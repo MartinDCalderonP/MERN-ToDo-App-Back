3ecffb0ba677f77cc4ce623b6ff4e1f7ab11ddde
Martín Calderón <martincalderon92@outlook.com>
2021-07-20 19:19:00 -0300

    Add MongoAtlas connection.

--- src/connection.js
+++ src/connection.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-const URI = 'mongodb://localhost/mern-todo-app';
+const URI =
+	'mongodb+srv://mdcp:Xq9Ga9AzLvb8abr@cluster0.mnqn4.mongodb.net/mern-todo-app-database?retryWrites=true&w=majority';
 
 mongoose
 	.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
